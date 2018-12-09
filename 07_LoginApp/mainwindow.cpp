720e5a78a4a5816507a045bd07047f4bb6091283
Vishnu R <vishnurapps@gmail.com>
2018-12-09 11:58:25 +0530

    Added login image

--- 07_LoginApp/mainwindow.cpp
+++ 07_LoginApp/mainwindow.cpp
@@ -1,12 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("/home/vishnu/Desktop/login.png");
+    ui->label_pic->setPixmap(pix.scaled(ui->label_pic->width(), ui->label_pic->height(), Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
